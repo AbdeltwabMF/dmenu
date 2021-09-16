74914966b16de1ab1ee105ec8880ce18e8a72804
AbdeltwabMF <abdeltwab.m.fakhry@gmail.com>
2021-09-16 06:29:21 +0200

    add awesome font

--- config.h
+++ config.h
@@ -5,9 +5,10 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
   "FantasqueSansMono-Regular:pixelsize=16:antialias=true:autohint=true"
-  "JoyPixels:pixelsize=16:antialias=true:autohint=true"
-  "Symbola:pixelsize=16:antialias=true:autohint=true"
+  "fontawesome-regular:pixelsize=18:antialias=true:autohint=true"
   "Ubuntu Arabic:pixelsize=16:antialias=true:autohint=true"
+  "JoyPixels:pixelsize=12:antialias=true:autohint=true"
+  "Symbola:pixelsize=12:antialias=true:autohint=true"
 };
 
 static const unsigned int bgalpha = 0xff;
@@ -15,9 +16,9 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                fg         bg       */
-	[SchemeNorm] = { "#a3af34", "#000000" },
-	[SchemeSel]  = { "#faaaa4", "#000000" },
-	[SchemeOut]  = { "#f3cff4", "#000000" },
+	[SchemeNorm] = { "#887FCF", "#2B2B2B" },
+	[SchemeSel]  = { "#E6EDF9", "#2B2B2B" },
+	[SchemeOut]  = { "#FFC19A", "#2B2B2B" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
