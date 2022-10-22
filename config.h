028696793ed4e9f301f849b3a8ee52dcafaec96a
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-10-22 23:21:57 +0200

    Fix icons and emoji

--- config.h
+++ config.h
@@ -5,15 +5,12 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
   "Fantasque Sans Mono:pixelsize=16:antialias=true:autohint=true",
-  "Ubuntu Arabic:pixelsize=16:antialias=true:autohint=true",
-  "SauceCodePro Nerd Font:pixelsize=16:antialias=true:autohint=true",
-  "FontAwesome:pixelsize=16:antialias=true:autohint=true",
-  "Pomodoro:pixelsize=16:antialias=true:autohint=true",
-  "octicons:pixelsize=16:antialias=true:autohint=true",
-  "icomoon:pixelsize=16:antialias=true:autohint=true",
-  "Twitter Color Emoji:pixelsize=16:antialias=true:autohint=true",
-  "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
-  "JoyPixels:pixelsize=16:antialias=true:autohint=true"
+	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=16:antialias=true:autohint=true",
+	"DejaVu Sans:pixelsize=16:antialias=true:autohint=true",
+	"Noto Sans Arabic:pixelsize=16:antialias=true:autohint=true",
+  "FuraMono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
+	"Amiri Quran Colored:pixelsize=16:antialias=true:autohint=true"
 };
 
 static const unsigned int bgalpha = 0xff;
